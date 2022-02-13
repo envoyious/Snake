ce3c7c690420c4e4d07377093d44d9a6baed13fa
abyssalEnvoy <zain.akram05@outlook.com>
2022-02-13 22:17:47 +0000

    Made STAThread

--- Minigames/Program.cs
+++ Minigames/Program.cs
@@ -4,6 +4,7 @@ namespace Minigames
 {
     public static class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             using (SnakeGame game = new SnakeGame())
